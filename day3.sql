50e64ee97678231d6f8eb82c45d87a5638c2a404
Swapnil Hingane <swapnilhingane9290@gmail.com>
2026-07-12 21:49:31 +0530

    check stmt use

--- day3.sql
+++ day3.sql
@@ -6,3 +6,16 @@ salary int DEFAULT 2500);
 
 INSERT INTO emp(id) VALUES (10);
 SELECT * FROM emp;
+ 
+CREATE TABLE city (
+id INT,
+cname VARCHAR(50),
+age INT,
+CONSTRAINT age_check CHECK (age>=18 AND cname="delhi")
+);
+
+INSERT INTO city (id,cname,age)
+VALUES(1,"delhi",19),
+(3,"DELHI",20),
+(4,"delhi",22);
+SELECT * FROM CITY;
